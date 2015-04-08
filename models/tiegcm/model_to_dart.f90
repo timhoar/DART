44a7b3d9fdc1ecdacd33c4030fe7ac9a14c6bbc5
Tim Hoar <thoar@ucar.edu>
2015-04-08 23:29:37 +0000

    Fixed a potentially bad error in that the variables were still being read from the restart file even when 'secondary' was specified. Cleaned up the logic specifying the model time. This also allows for a more accurate 'location' of the f10_7 parameter, which is best described as local noon. This cannot be determined 'once' by static_init_model, for example - and the model_mod (in general) does not have access to the current time. Turns out - every time the model is advanced the ens_mean_for_model() is recomputed, so the model_time is re-read from the restart file in ens_mean_for_model().
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/tiegcm@7866 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 82f9b6309c33f7f559a70912e041c2644a2b0b67

--- models/tiegcm/model_to_dart.f90
+++ models/tiegcm/model_to_dart.f90
@@ -20,8 +20,7 @@ use    utilities_mod, only : get_unit, initialize_utilities, finalize_utilities,
                              error_handler, E_MSG, &
                              find_namelist_in_file, check_namelist_read
 use        model_mod, only : static_init_model, get_model_size, &
-                             read_TIEGCM_restart, get_restart_file_name
-!                            clamp_bounded_variables
+                             tiegcm_to_dart_vector
 use  assim_model_mod, only : open_restart_write, awrite_state_restart, close_restart
 use time_manager_mod, only : time_type, print_date, print_time
 
@@ -37,7 +36,7 @@ character(len=128), parameter :: revdate  = "$Date$"
 ! namelist parameters with default values.
 !-----------------------------------------------------------------------
 
-character(len=128) :: file_out = 'dart_ics'
+character(len=256) :: file_out = 'dart_ics'
 
 namelist /model_to_dart_nml/ file_out
 
@@ -45,7 +44,6 @@ namelist /model_to_dart_nml/ file_out
 ! global storage
 !-----------------------------------------------------------------------
 
-character(len=128)    :: filename
 type(time_type)       :: model_time
 integer               :: iunit, io, x_size
 real(r8), allocatable :: x_state(:)
@@ -69,15 +67,12 @@ allocate(x_state(x_size))
 
 ! Read the TIEGCM state variables into var and set the model_time
 ! to reflect the valid time of the TIEGCM state.
+! The origin of the variables comes from the information specified
+! in input.nml:&model_nml:variables   
 
-filename = get_restart_file_name()
+call tiegcm_to_dart_vector(x_state, model_time)
 
-call read_TIEGCM_restart(trim(filename), x_state, model_time)
-
-! clamp the bounded variables before writing to TIEGCM restart file.
-! call clamp_bounded_variables(x_state)
-
-! write out state vector in "proprietary" format
+! write out state vector in DART format
 iunit = open_restart_write(trim(file_out))
 call awrite_state_restart(model_time, x_state, iunit)
 call close_restart(iunit)
